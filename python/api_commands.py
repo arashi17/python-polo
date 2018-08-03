d6e7962220bf7636fc9fbb7585520da1d32c2882
arashi17 <hirata.fabio@gmail.com>
2018-08-04 00:36:18 +1000

    Add get_ticker command

--- python/api_commands.py
+++ python/api_commands.py
@@ -1,2 +1,18 @@
-""" Connections """
+""" API Commands """
+import requests
+import json
+
+api_urls = {'polo' : 'https://poloniex.com/public?command=', 'bitt' : 'https://bittrex.com/api/v1.1/', 'cryp' : 'https://www.cryptopia.co.nz/api/'}
+
+def get_ticker(exchange):
+	if(exchange == 'bitt'):
+		answer = requests.get(api_urls['bitt'] + 'public/getmarketsummaries')
+		ticker = answer.json()
+	if(exchange == 'polo'):
+		answer = requests.get(api_urls['polo'] + 'returnTicker')
+		ticker = answer.json()
+	if(exchange == 'cryp'):
+		answer = requests.get(api_urls['cryp'] + 'GetMarkets')
+		ticker = answer.json()
+	return(ticker)
 
