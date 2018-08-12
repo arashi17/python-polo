e670acd7d6185406f48e8fd8a6d0f6ae3e8b497c
arashi17 <hirata.fabio@gmail.com>
2018-08-12 22:19:12 +1000

    Fix to return float instead of string

--- python/polo_api.py
+++ python/polo_api.py
@@ -20,13 +20,14 @@ def get_data():
 def get_order_book(pair):
   answer = requests.get(api_url + 'returnOrderBook&currencyPair=' + pair + '&depth=30')
   order_book = answer.json()
+  print(order_book)
   bid_list = []
   ask_list = []
   price_list = {}
   for i in range(len(order_book['bids'])):
-    bid_list.append(order_book['bids'][i])
+    bid_list.append([float(order_book['bids'][i][0]), order_book['bids'][i][1]])
   for i in range(len(order_book['asks'])):
-    ask_list.append(order_book['asks'][i])
+    ask_list.append([float(order_book['asks'][i][0]), order_book['asks'][i][1]])
   price_list['Bid'] = bid_list
   price_list['Ask'] = ask_list
   return price_list
