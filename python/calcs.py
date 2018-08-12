650a1fce5ba44916e4c512358c8c1b2512bc3904
arashi17 <hirata.fabio@gmail.com>
2018-08-12 15:21:21 +1000

    Calculates inverted values

--- python/calcs.py
+++ python/calcs.py
@@ -13,10 +13,27 @@ def opp_search(pairs_dict):
 			found_pair = []
 			# print(pair_list)
 			for i in range(len(bid_list)):
+				bid_price = bid_list[i]
+				bid_inverted = False
+				if bid_price < 1:
+					bid_price = 1 / bid_price
+					bid_inverted = True
 				for j in range(len(ask_list)):
-					if bid_list[i] > ask_list[j]:
-						found_pair.append(pair_list[i])
-						print(pair_list)
-						print(str(i) + ' ' + str(j) + ' ' + str(bid_list[i]) + ' ' + str(ask_list[j]))
-					break
+					ask_price = ask_list[j]
+					ask_inverted = False
+					if ask_price < 1:
+						ask_price = 1 / ask_price
+						ask_inverted = True
+					if bid_inverted and ask_inverted:
+						if bid_price < ask_price:
+							found_pair.append(pair_list[i])
+							print(pair_list)
+							print(str(i) + ' ' + str(j) + ' ' + str(bid_list[i]) + ' ' + str(ask_list[j]) + ' ' + str(bid_price) + ' ' + str(ask_price))
+						break
+					else:
+						if bid_price > ask_price:
+							found_pair.append(pair_list[i])
+							print(pair_list)
+							print(str(i) + ' ' + str(j) + ' ' + str(bid_list[i]) + ' ' + str(ask_list[j]) + ' ' + str(bid_price) + ' ' + str(ask_price))
+						break
 		# print(found_pair)
