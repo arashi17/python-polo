216eb14fe45a7d2e11146855d66aac4371bbb92e
arashi17 <hirata.fabio@gmail.com>
2018-08-21 23:20:55 +1000

    Add command parsing, add filtering (not working)

--- python/analyse.py
+++ python/analyse.py
@@ -1,30 +1,60 @@
 """ Calculates profit from json data
 """
 
+import argparse
+import fnmatch
 import requests
 import json
 import os
 
-path = './data/'
-loaded_data = []
-total_profit = {}
-for filename in os.listdir(path):
-  with open(path + filename) as f:
-    loaded_data.append(json.load(f))
+parser = argparse.ArgumentParser(description = 'Generate report from all files in the folder.')
+parser.add_argument('--path')
+parser.add_argument('--blues')
+args = vars(parser.parse_args())
 
-for i in range(len(loaded_data)):
-  for legend in loaded_data[i].keys():
-    if legend in total_profit.keys():
-      total_profit[legend][0] += loaded_data[i][legend][0]
-    else:
-      total_profit[legend] = [loaded_data[i][legend][0], loaded_data[i][legend][1]]
-    loaded_data[i][legend][0] = 0
-    j = 1
-    while (i + j) < (len(loaded_data) - 1):
-      if legend in loaded_data[i + j].keys():
-        total_profit[legend][0] += loaded_data[i + j][legend][0]
-        loaded_data[i + j][legend][0] = 0
-      j += 1
+
+path = args['path']
+loaded_data = []
+loaded_data_filtered = []
+temp_dict = {}
+total_profit = {}
+removed_pairs = ['BITS!BTC', 'BTC!BITS', 'BTC!BTM', 'BTM!BTC', 'BTC!FCT', 'FCT!BTC', 'BTC!BTG', 'BTG!BTC', 'EMC2!BTC', 'BTC!EMC2', 'EOS!BTC', 'BTC!EOS']
+considered = ['BTC', 'USDT', 'ETH', 'ETC', 'LTC', 'XMR', 'DASH', 'XRP']
+considered_list = []
+for i in range(len(considered)):
+  for j in range((i + 1), len(considered)):
+    considered_list.append(considered[i] + '!' + considered[j])
+    considered_list.append(considered[j] + '!' + considered[i])
+filelist = os.listdir(path)
+filtered_filelist = fnmatch.filter(filelist, 'res_*')
+for filename in filelist:
+  if filename not in filtered_filelist:
+    with open(path + filename) as f:     
+      loaded_data.append(json.load(f))
+print(args['blues'])
+if args['blues'] == 'Y':
+  for i in range(len(loaded_data)):
+    for legend in loaded_data[i].keys():
+      if legend in considered_list:
+        temp_dict[legend] = loaded_data[i][legend]
+    loaded_data_filtered.append(temp_dict)
+else:
+  loaded_data_filtered = loaded_data.copy()
+
+for i in range(len(loaded_data_filtered)):
+  for legend in loaded_data_filtered[i].keys():
+    if legend not in removed_pairs:
+      if legend in total_profit.keys():
+        total_profit[legend][0] += loaded_data_filtered[i][legend][0]
+      else:
+        total_profit[legend] = [loaded_data_filtered[i][legend][0], loaded_data_filtered[i][legend][1]]
+      loaded_data_filtered[i][legend][0] = 0
+      j = 1
+      while (i + j) < (len(loaded_data_filtered) - 1):
+        if legend in loaded_data_filtered[i + j].keys():
+          total_profit[legend][0] += loaded_data_filtered[i + j][legend][0]
+          loaded_data_filtered[i + j][legend][0] = 0
+        j += 1
 
 consolidated = {}
 for legend in total_profit:
@@ -33,8 +63,8 @@ for legend in total_profit:
   else:
     consolidated[total_profit[legend][1]] = total_profit[legend][0]
 
-with open(path + 'analyse.json', 'w') as outcome:
+with open(path + 'res_analyse.json', 'w') as outcome:
   json.dump(total_profit, outcome)
 
-with open(path + 'consolidated.json', 'w') as outcome:
+with open(path + 'res_consolidated.json', 'w') as outcome:
   json.dump(consolidated, outcome)
--- python/run.py
+++ python/run.py
@@ -1,5 +1,7 @@
 """ Execute """
 
+import argparse
+import os
 import cryp_api
 import bitt_api
 import polo_api
@@ -7,41 +9,52 @@ import org_data
 import json
 import time
 
-def run(i):
+parser = argparse.ArgumentParser(description='Run.')
+parser.add_argument('--scan_time', type=int)
+parser.add_argument('--interval', type=int)
+parser.add_argument('--path')
+
+args = vars(parser.parse_args())
+
+def run(i, path):
   polo = polo_api.get_data()
   bitt = bitt_api.get_data()
   cryp = cryp_api.get_data()
 
   prices = org_data.compare_exc(polo, bitt)
   total_profit = org_data.profit_calc(prices)
-  with open('./data/polo_bitt' + str(i) + '.json', 'w') as outfile:
+  with open(path + 'polo_bitt' + str(i) + '.json', 'w') as outfile:
     json.dump(total_profit, outfile)
 
   prices = org_data.compare_exc(polo, cryp)
   total_profit = org_data.profit_calc(prices)
-  with open('./data/polo_cryp' + str(i) + '.json', 'w') as outfile:
+  with open(path + 'polo_cryp' + str(i) + '.json', 'w') as outfile:
     json.dump(total_profit, outfile)
 
   prices = org_data.compare_exc(bitt, cryp)
   total_profit = org_data.profit_calc(prices)
-  with open('./data/bitt_cryp' + str(i) + '.json', 'w') as outfile:
+  with open(path + 'bitt_cryp' + str(i) + '.json', 'w') as outfile:
     json.dump(total_profit, outfile)
 
 
-total_scan_time = 7 # in hours
-interval = 10 # in minutes
+path = args['path']
+if not os.path.exists(path):
+  os.makedirs(path)
+total_scan_time = args['scan_time'] # in hours
+interval = args['interval'] # in minutes
 scans = total_scan_time * 60 / interval
 start_time = time.time()
 for i in range(int(scans)):
   run_time = time.time()
   print('Run %d out of %d' % (i + 1, int(scans)))
-  run(i)
+  run(i, path)
   elapsed_time = time.time() - run_time
   interval_sec = (interval * 60) - elapsed_time
   if interval_sec <= 0:
     interval_sec = 10
   print('Sleeping for %d seconds' % interval_sec)
-  time.sleep(interval_sec)
+  if i < (int(scans) - 1):
+    time.sleep(interval_sec)
   end_run = time.time() - run_time
   print('Run %d took %d seconds' % (i + 1, end_run))
 total_time = (time.time() - start_time) / 60
