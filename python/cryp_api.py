276531813acef4d72d7d61c76df5cbb485f8c7fd
arashi17 <hirata.fabio@gmail.com>
2018-09-01 23:43:49 +1000

    Add initial private cryp api functions

--- python/cryp_api.py
+++ python/cryp_api.py
@@ -3,6 +3,13 @@
 import requests
 import json
 
+from time import time
+import hmac
+import hashlib
+import base64
+from requests.compat import quote_plus
+
+
 api_url = 'https://www.cryptopia.co.nz/api/'
 
 def legend2pair(legend):
@@ -57,3 +64,34 @@ def get_order_book(legend, depth, inverted):
   price_dict['Ask'] = ask_list
   return price_dict
   
+
+class Cryp:
+  def __init__(self, api_key, api_secret):
+    self.api_key = api_key
+    self.api_secret = api_secret
+    self.url = 'https://www.cryptopia.co.nz/Api/'
+
+
+  def secure_headers(self, url, payload):
+    """ Creates secure header for cryptopia private api. """
+    nonce = str(time())
+    md5 = hashlib.md5()
+    jsonparams = payload.encode('utf-8')
+    md5.update(jsonparams)
+    rcb64 = base64.b64encode(md5.digest()).decode('utf-8')
+    
+    signature = self.api_key + "POST" + quote_plus(url).lower() + nonce + rcb64
+    hmacsignature = base64.b64encode(hmac.new(base64.b64decode(self.api_secret),
+                                              signature.encode('utf-8'),
+                                              hashlib.sha256).digest())
+    header_value = "amx " + self.api_key + ":" + hmacsignature.decode('utf-8') + ":" + nonce
+    return {'Authorization': header_value, 'Content-Type': 'application/json; charset=utf-8'}
+
+
+  def return_balance(self, currency):
+    url_add = 'GetBalance'
+    url = self.url + url_add
+    payload = str({'Currency' : currency})
+    headers = self.secure_headers(url=url, payload=payload)
+    r = requests.post(url, data=payload, headers=headers)
+    print(r.json())
